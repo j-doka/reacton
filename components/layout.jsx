16a53b3d8c781c027ea370ec32076f08c0a065d1
chukkyiii <jgodwin1420@gmail.com>
2021-08-28 19:53:30 +0100

    Frogot to add a style change to the title

--- components/layout.jsx
+++ components/layout.jsx
@@ -4,6 +4,7 @@ import {
     container,
     highlight,
     nav_container,
+    title_style
 } from './layout.module.css'
 
 const Layout = ({children, PageTitle}) => {
@@ -13,10 +14,10 @@ const Layout = ({children, PageTitle}) => {
                 <title>{PageTitle}</title>
                 <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;1,300;1,400;1,500&display=swap" rel="stylesheet"></link>
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+                <link rel="preconnect" href="https://fonts.gstatic.com" />
             </Head>
             <div className={container}>
-                <h1>{PageTitle}<span className={highlight}>{/* add styling to the .tech*/}.tech</span></h1>
+                <h1 className={title_style}>{PageTitle}<span className={highlight}>{/* add styling to the .tech*/}.tech</span></h1>
                 <header className={nav_container}>
                     <nav>
                         {/* Add Links to each one (tick )*/}
