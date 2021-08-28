ad17fb05ea20ac60501a73825b9faa66eefc4baa
chukkyiii <jgodwin1420@gmail.com>
2021-08-28 19:59:46 +0100

    Added reaction box component

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,12 @@
-import Head from 'next/head'
-import Link from 'next/link'
 import Layout from '../components/layout.jsx'
+import Reactionbox from '../components/Reactionbox.jsx'
 
 const IndexPage = () => {
   return (
     <Layout PageTitle='Reacton'>
+      <Reactionbox>
 
+      </Reactionbox>
     </Layout>
   )
 }
