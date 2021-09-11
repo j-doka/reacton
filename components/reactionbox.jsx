8a087952f731d04e07214bb00e5504f703314945
chukkyiii <jgodwin1420@gmail.com>
2021-09-12 00:35:26 +0100

    Added centerd image for default page

--- components/reactionbox.jsx
+++ components/reactionbox.jsx
@@ -1,3 +1,5 @@
+import Image from 'next/image'
+import Lightning from '../public/icons8-lightning-bolt-96.png'
 import { 
     test_container,
     reac_container,
@@ -5,24 +7,28 @@ import {
     prev_reac,
     curr_reac,
     reset_button,
+    lightning
 } from './reactionbox.module.css'
 
 const Reactionbox = () => {
     return(
         <div className={test_container}>
+            
             <div className={reac_container}>
-
+                <Image className={lightning} src={Lightning} alt="Lightning Symbol" />
             </div>
             <div className={test_bar}>
                 <div className={prev_reac}>
                     {/* Should contain previous Speed or '-' to signify nothing has been recorded yet*/}
                 </div>
+                
                 <div className={curr_reac}>
                     {/* Time elapsed? */}
                 </div>
                 <button className={reset_button}>
                     {/* Start Again */}
                 </button>
+                
             </div>
         </div>
     )
