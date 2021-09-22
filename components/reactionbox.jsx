356c883c3c6d54eaabc991f00894783c160d6371
chukkyiii <jgodwin1420@gmail.com>
2021-09-22 14:00:39 +0100

    changed reactionbox into a react class component

--- components/reactionbox.jsx
+++ components/reactionbox.jsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import { Component } from 'react'
 import Lightning from '../public/icons8-lightning-bolt-96.png'
 import Refresh from '../public/icons8-refresh-480.png'
 import { 
@@ -7,12 +8,55 @@ import {
     test_bar,
     bar_item,
     reset_button,
-    refresh
 } from './reactionbox.module.css'
 
-const Reactionbox = () => {
-    return(
-        <div className={test_container}>
+class Reactionbox extends Component {
+    // timerOn: is the Timer On?
+    // TimerStart:  ??? 
+    // TimerTime totaTime elapsed
+    state = {
+        timerOn: false,
+        timerStart: 0,
+        timerTime: 0
+    }
+
+    startTimer = () => {
+        this.setState({
+            timerOn: True,
+            timerTime: this.state.timerTime,
+            timerStart: Date.now() - this.state.timerTime           
+        })
+        this.timer = setInterval(() => {
+            this.setState({
+                timerTime: Date.now() - this.state.timerStart
+            })
+        }, 10)
+    }
+
+    stopTimer = () => {
+        this.setState({ timerOn: flase })
+        clearInterval(this.timer)
+    }
+
+    
+
+    resetTimer = () => {
+        this.setState({
+            timerStart: 0,
+            timerTime: 0
+        })
+    }
+
+    render(){
+
+        const { timerTime } = this.state
+        let centiseconds = ("0" + (Math.floor(timerTime / 10) % 100)).slice(-2)
+        let seconds = ("0" + (Math.floor(timerTime / 1000) % 60)).slice(-2)
+        let minutes = ("0" + (Math.floor(timerTime / 60000) % 60)).slice(-2)
+        let hours = ("0" + Math.floor(timerTime / 3600000)).slice(-2)
+        
+        return(
+            <div className={test_container}>
             
             <div className={reac_container}>
                 <Image src={Lightning} alt="Lightning Symbol" />
@@ -22,14 +66,17 @@ const Reactionbox = () => {
                     {/* Should contain previous Speed or '-' to signify nothing has been recorded yet*/}
                 </div>
                 <div className={bar_item}>
-                    {/* Time elapsed? */}
+                    <div>
+                        <div>Stopwatch</div>
+                    </div>
                 </div>
                 <button className={reset_button}>
                     <Image width={45} height={45} src={Refresh} alt="Refresh Symbol"/>
                 </button>
             </div>
         </div>
-    )
+        )
+    }
 }
 
 export default Reactionbox
