59e325574a245d6a8b01d05518b574627cbc2903
chukkyiii <jgodwin1420@gmail.com>
2021-09-26 01:01:24 +0100

    timer automatically starts: bug: timer does not set back to 0

--- components/reactionbox.jsx
+++ components/reactionbox.jsx
@@ -13,13 +13,11 @@ import {
 } from './reactionbox.module.css'
 
 class Reactionbox extends Component {
-    // timerOn: is the Timer On?
-    // TimerStart:  ??? 
-    // TimerTime totaTime elapsed
     state = {
         timerOn: false,
         timerStart: 0,
         timerTime: 0,
+        auto: false,
         ready: false,
         go: false, // logic
     }
@@ -37,7 +35,8 @@ class Reactionbox extends Component {
         this.setState({
             timerOn: true,
             timerTime: this.state.timerTime,
-            timerStart: Date.now() - this.state.timerTime,      
+            timerStart: Date.now() - this.state.timerTime,
+            auto: true    
         })
         this.timer = setInterval(() => {
             this.setState({
@@ -54,7 +53,10 @@ class Reactionbox extends Component {
     resetTimer = () => {
         this.setState({
             timerStart: 0,
-            timerTime: 0
+            timerTime: 0,
+            auto: false,
+            ready: false,
+            go: false
         })
         clearInterval(this.timer)
     }
@@ -64,27 +66,45 @@ class Reactionbox extends Component {
         
     }
 
+    componentDidMount() {
+        setInterval(() => {
+            if (this.state.go === true){
+                document.getElementById("ready").click()
+            }
+        }, 5) 
+    }
+
     render(){
 
         let { timerTime } = this.state
         let { ready } = this.state
         let { go } = this.state // responislbe for setting image to green
+        let { auto } = this.state 
+        let goTime = null
         let centiseconds = ("0" + (Math.floor(timerTime / 10) % 100)).slice(-2)
         let seconds = ("0" + (Math.floor(timerTime / 1000) % 60)).slice(-2)
         let minutes = ("0" + (Math.floor(timerTime / 60000) % 60)).slice(-2)
         let hours = ("0" + Math.floor(timerTime / 3600000)).slice(-2)
 
+        // I got a bit stuck, we need it to measure the amount of time it took to react to the green box!
+        // how do we do that!
+
+        if (go === true){
+            goTime = 'ready'
+        }
+
         return(
             <div className={test_container}>
-                <button className={ go === true ? goState : ready === true ? readyState : reac_container} onClick={go === true ? this.stopTimer : this.readyState } >
+                <button id={goTime} className={ go === true ? goState : ready === true ? readyState : reac_container} onClick={go === true ? this.startTimer : auto === true ? this.stopTimer : this.readyState } >
                     {ready == true ? '' : <Image src={Lightning} alt="Lightning Symbol" />}
                 </button>
                 <div className={test_bar}>
                     <div className={bar_item}>
                         {/* Should contain previous Speed or '-' to signify nothing has been recorded yet*/}
                     </div>
-                    <div className={bar_item} onLoad={go === true ? '' : this.startTimer}>
+                    <div className={bar_item} >
                         <div>
+                            {console.log(timerTime, ready, go, goTime, auto)}
                             {hours} : {minutes} : {seconds} : {centiseconds}
                         </div>
                     </div>
