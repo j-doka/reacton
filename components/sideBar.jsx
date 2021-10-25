0f762e3f42c006371a92f282ab21a9944e3ba3fb
chukkyiii <jgodwin1420@gmail.com>
2021-10-25 20:04:46 +0100

    Added dynamic heading

--- components/sideBar.jsx
+++ components/sideBar.jsx
@@ -1,20 +1,29 @@
 import React from 'react'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
+import { supabase } from '../utils/supabaseClient'
 import {
     non_active,
     active,
     nav_links
 } from './sidebar.module.css'
 
-const menu = [
-    { title: 'Test', path: '/' },
-    { title: 'About', path: '/about' },
-    { title: 'Log In', path: '/login' },
-]
+
+
 
 const Sidebar = () => {
     const router = useRouter()
+
+    const session = supabase.auth.session()
+    let Login
+    session ? Login = session.user.user_metadata.full_name : Login = 'Log In'
+
+    const menu = [
+        { title: 'Test', path: '/' },
+        { title: 'About', path: '/about' },
+        { title: Login, path: '/login' },
+    ]
+
 
     return (
         <ul className={nav_links}>
