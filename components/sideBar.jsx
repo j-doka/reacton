05e1c3182eac12866ada0f76c373108a9c9bc7d6
chukkyiii <jgodwin1420@gmail.com>
2021-10-26 17:54:58 +0100

    Will add about page later

--- components/sideBar.jsx
+++ components/sideBar.jsx
@@ -20,7 +20,6 @@ const Sidebar = () => {
 
     const menu = [
         { title: 'Test', path: '/' },
-        { title: 'About', path: '/about' },
         { title: Login, path: '/login' },
     ]
 
