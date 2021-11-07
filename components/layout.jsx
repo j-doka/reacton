50adfbcfeb7cbc75beb75d489c60233e8a0f5d07
chuks <jgodwin1420@gmail.com>
2021-11-07 21:32:38 +0000

    made a new favicon looking nice!

--- components/layout.jsx
+++ components/layout.jsx
@@ -15,6 +15,7 @@ const Layout = ({children, PageTitle}) => {
             <Head>
                 <title>{PageTitle}</title>
                 <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;1,300;1,400;1,500&display=swap" rel="stylesheet"></link>
+                <link rel="shortcut icon" href="/favicon.svg" />
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" />
             </Head>
