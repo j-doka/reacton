a579d0ec63df0321a4029565eb0a4a8dc9e3612a
chuks <jgodwin1420@gmail.com>
2022-02-27 09:06:18 +0000

    read data from database

--- pages/login/index.js
+++ pages/login/index.js
@@ -4,9 +4,10 @@ import Auth from '../../components/Auth'
 import Account from '../../components/Account'
 import Layout from '../../components/layout.jsx'
 
-export default function Home() {
+export default function Home({times}) {
     const [session, setSession] = useState(null)
-
+    console.log(times)
+    // iterate through data and assign each data to the repective user. 
     useEffect(() => {
         setSession(supabase.auth.session())
 
@@ -18,8 +19,28 @@ export default function Home() {
     return (
         <Layout PageTitle='Reacton'>
             <div className="container" style={{ padding: '50px 0 100px 0' }}>
-                {!session ? <Auth /> : <Account key={session.user.id} session={session} />}
+                { !session 
+                    ? <Auth /> 
+                    : <Account key={session.user.id} session={session}>
+                        <div>
+                            {/* graph */}
+                        </div>
+                    </Account>
+                
+                }
             </div>
         </Layout>
     )
+}
+
+export const getStaticProps = async () => {
+    const { data: times } = await supabase
+        .from('profiles')
+        .select(`timerDelt, username`)
+
+    return {
+        props: {
+            times,
+        }
+    }
 }
