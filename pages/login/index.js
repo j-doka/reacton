a827ef3ccf2701c5afe1e5e54dbf725e1233dae8
chukkyiii <jgodwin1420@gmail.com>
2022-03-03 23:16:37 +0000

    Revert "the personal graph is now here!"
    This reverts commit ac170f3634e2da81962614b68475263dfe3c5730.

--- pages/login/index.js
+++ pages/login/index.js
@@ -3,23 +3,10 @@ import { supabase } from '../../utils/supabaseClient'
 import Auth from '../../components/Auth'
 import Account from '../../components/Account'
 import Layout from '../../components/layout.jsx'
-import { Line } from 'react-chartjs-2';
-
-// let chart = {
-//     width: '1000px',
-//     height: '1000px',
-//     margin: 'auto',
-//     padding: '10px'
-// }
 
 export default function Home({times}) {
     const [session, setSession] = useState(null)
-    const [loading, setLoading] = useState(null)
-    const [dtimes] = useState([])
     console.log(times)
-    //  filter all times that are associated with this account
-    
-    
     // iterate through data and assign each data to the repective user. 
     useEffect(() => {
         setSession(supabase.auth.session())
@@ -27,60 +14,16 @@ export default function Home({times}) {
         supabase.auth.onAuthStateChange((_event, session) => {
             setSession(session)
         })
-        for (let i = 0; i < times.length; i++){
-            console.log(times[i].timerDelt)
-            if (times[i].username == supabase.auth.currentUser.user_metadata.full_name){
-                dtimes.push(times[i].timerDelt)
-            }
-            else {
-                times.splice(i)
-            }
-        }
-
-    }, [times, dtimes])
-
-    let tries = []
-    for (let i = 0; i <= dtimes.length; i++){
-        tries.push(i)
-    }
-
-    const state = {
-        labels: tries,
-        datasets: [
-            {
-                label: 'Reaction',
-                fill: false,
-                lineTension: 0.5,
-                backgroundColor: 'rgba(75,192,192,1)',
-                borderColor: 'rgba(78, 172, 207, 100)',
-                borderWidth: 2,
-                data: dtimes
-            }
-        ]
-    }
-
+    }, [])
 
     return (
         <Layout PageTitle='Reacton'>
             <div className="container" style={{ padding: '50px 0 100px 0' }}>
                 { !session 
                     ? <Auth /> 
-                    : <Account key={session.user.id} session={session}> 
+                    : <Account key={session.user.id} session={session}>
                         <div>
-                            <Line
-                                data={state}
-                                options={{
-                                    title: {
-                                        display: true,
-                                        text: 'Average Reaction Time',
-                                        fontSize: 20
-                                    },
-                                    legend: {
-                                        display: true,
-                                        position: 'right'
-                                    }
-                                }}
-                            />
+                            {/* graph */}
                         </div>
                     </Account>
                 
@@ -94,7 +37,6 @@ export const getStaticProps = async () => {
     const { data: times } = await supabase
         .from('profiles')
         .select(`timerDelt, username`)
-        // .eq(`username`, `${supabase.auth.currentUser.user_metadata.full_name}`)
 
     return {
         props: {
