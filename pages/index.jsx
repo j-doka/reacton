cd312bd52b5094c8c11c463739ec5e4762ff8d41
chukkyiii <jgodwin1420@gmail.com>
2022-03-03 23:10:58 +0000

    Revert "go on the computer it only works on computer (on purpose)"
    This reverts commit 48ae2209d449db6b89c188932d4e0380075761e4.

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,43 +1,13 @@
 import Layout from '../components/layout.jsx'
 import Reactionbox from '../components/reactionbox.jsx'
-import useWindowDimensions from '../hooks/useWindowDimensions'
-
-
-let announce = {
-  textAlign: 'center',
-  padding: '1.5rem',
-  borderRadius: '4px',
-  backgroundColor: '#2a2a2a',
-  display: 'flex',
-  justifyContent: 'center',
-  
-}
-
-let container = {
-    display: 'flex',
-    width: '100%',
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'column',
-    transition: 'all 0.2s ease 0s',
-}
 
 const IndexPage = () => {
-  const { windowWidth } = useWindowDimensions();
-  const bool = Boolean(windowWidth >= 600)
   return (
-    <>{ bool ? 
-      <Layout PageTitle='Reacton'>
-        <Reactionbox>
-        </Reactionbox>
-      </Layout> : 
-      <div style={container}>
-        <div style={announce}>
-          <p>Stand up and go to your computer...</p>
-        </div>
-      </div>
-      }
-    </>
+    <Layout PageTitle='Reacton'>
+      <Reactionbox>
+      </Reactionbox>
+      
+    </Layout>
   )
 }
 
